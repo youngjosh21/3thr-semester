1003888b3bc6ca2f5e80b40b503a824015485498
youngjosh21 <143547673+youngjosh21@users.noreply.github.com>
2024-09-17 16:54:25 -0300

    Update Main.java

--- App/src/main/Main.java
+++ App/src/main/Main.java
@@ -20,6 +20,34 @@ String nome = sc.nextLine();
             System.out.println("Digite a quantidade do produto:");
             int Quantidade = sc.nextInt();
 
+////
+         //ligando o objeto
+        Entidade produto = new Produto(nome, preco, quantidade);
+
+        //mostrando dados do produto
+        System.out.println(produto);
+
+        //entrada produto
+        System.out.println("Adicionar mais produtos");
+        System.out.print("Informe a quantidade para adicionar: ");
+        quantidade = sc.nextInt();
+
+        //metodo adicionar quantidade
+        produto.addProduto(quantidade);
+
+        //mostrando dados do produto com nova quantidae
+        System.out.println(produto);
+
+        //saida produto
+        System.out.println("Saida dos produtos");
+        System.out.print("Informe a quantidade para retirar: ");
+        quantidade = sc.nextInt();
+
+        //metodo retirar quantidade
+        produto.removerProduto(quantidade);
+
+        //mostrando dados do produto com nova quantidae
+        System.out.println(produto);
 
 
     }
