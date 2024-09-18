e0e1322d48dad07f56fa65a611d7754dcb320cde
youngjosh21 <143547673+youngjosh21@users.noreply.github.com>
2024-09-18 14:11:46 -0300

    Atualizar o Main.java

--- App/src/main/Main.java
+++ App/src/main/Main.java
@@ -9,8 +9,7 @@ import Produto.Produto;
 
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        
         Scanner sc = new Scanner(System.in) ;
 
             System.out.println("Digite o nome do produto:");
